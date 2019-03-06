650d82b140714347748a480eafcc0edfd9f1425f
kvatoferma <jimmaggot@mail.ru>
2019-03-07 01:24:20 +0300

    fix: delete addInfo function

--- src/orders/renderTableBody.js
+++ src/orders/renderTableBody.js
@@ -1,7 +1,5 @@
 import createOrderRow from "./createRow";
 import createDOMElement from "../DOM/createElement";
-import addUserInfo from "../users/addInfo"
-import users from "../../data/users.json"
 
 export default function (orders) {
   let tbody  = document.querySelector('tbody');
@@ -13,9 +11,4 @@ export default function (orders) {
   orders.forEach(order => {
     createOrderRow(order, 'tbody');
   });
-  orders.forEach(order => {
-    users.forEach(user => {
-      addUserInfo(order, user);
-    });
-  });  
 }
